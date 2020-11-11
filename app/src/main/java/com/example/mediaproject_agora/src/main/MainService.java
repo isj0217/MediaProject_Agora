696ea6f49bfae79c7f8c1d56fb04cca1eadb3ec2
조인성 <insung@joinseong-ui-MacBookPro.local>
2020-11-11 22:42:28 +0900

    20201111 마이그레이션을 위한 초기 커밋

---
+++ app/src/main/java/com/example/mediaproject_agora/src/main/MainService.java
@@ -0,0 +1,40 @@
+package com.example.mediaproject_agora.src.main;
+
+import com.example.mediaproject_agora.src.main.interfaces.MainActivityView;
+import com.example.mediaproject_agora.src.main.interfaces.MainRetrofitInterface;
+import com.example.mediaproject_agora.src.main.models.DefaultResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.example.mediaproject_agora.src.ApplicationClass.getRetrofit;
+
+class MainService {
+    private final MainActivityView mMainActivityView;
+
+    MainService(final MainActivityView mainActivityView) {
+        this.mMainActivityView = mainActivityView;
+    }
+
+    void getTest() {
+        final MainRetrofitInterface mainRetrofitInterface = getRetrofit().create(MainRetrofitInterface.class);
+        mainRetrofitInterface.getTest().enqueue(new Callback<DefaultResponse>() {
+            @Override
+            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
+                final DefaultResponse defaultResponse = response.body();
+                if (defaultResponse == null) {
+                    mMainActivityView.validateFailure(null);
+                    return;
+                }
+
+                mMainActivityView.validateSuccess(defaultResponse.getMessage());
+            }
+
+            @Override
+            public void onFailure(Call<DefaultResponse> call, Throwable t) {
+                mMainActivityView.validateFailure(null);
+            }
+        });
+    }
+}
