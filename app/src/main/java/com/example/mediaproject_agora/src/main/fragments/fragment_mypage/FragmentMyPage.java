977688d37b03a0d2fc714f221c86edc1087db85b
조인성 <insung@joinseong-ui-MacBookPro.local>
2020-12-04 20:15:20 +0900

    로그아웃 기능 구현

--- app/src/main/java/com/example/mediaproject_agora/src/main/fragments/fragment_mypage/FragmentMyPage.java
+++ app/src/main/java/com/example/mediaproject_agora/src/main/fragments/fragment_mypage/FragmentMyPage.java
@@ -1,5 +1,6 @@
 package com.example.mediaproject_agora.src.main.fragments.fragment_mypage;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -22,8 +23,11 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import com.example.mediaproject_agora.R;
+import com.example.mediaproject_agora.src.main.DepartmentBoardActivity;
+import com.example.mediaproject_agora.src.main.InPostActivity;
 import com.example.mediaproject_agora.src.main.LikePostsActivity;
 import com.example.mediaproject_agora.src.main.models.DefaultResponse;
+import com.example.mediaproject_agora.src.sign_in.SignInActivity;
 
 import java.io.IOException;
 import java.net.URL;
@@ -40,6 +44,7 @@ public class FragmentMyPage extends Fragment implements FragmentMyPageView {
     private LinearLayout ll_mypage_change_nickname;
     private LinearLayout ll_mypage_change_profile_photo;
     private LinearLayout ll_mypage_like_posts;
+    private LinearLayout ll_mypage_log_out;
 
     @Nullable
     @Override
@@ -65,6 +70,7 @@ public class FragmentMyPage extends Fragment implements FragmentMyPageView {
         ll_mypage_change_nickname = viewGroup.findViewById(R.id.ll_mypage_change_nickname);
         ll_mypage_change_profile_photo = viewGroup.findViewById(R.id.ll_mypage_change_profile_photo);
         ll_mypage_like_posts = viewGroup.findViewById(R.id.ll_mypage_like_posts);
+        ll_mypage_log_out = viewGroup.findViewById(R.id.ll_mypage_log_out);
     }
 
     public void setClickListeners() {
@@ -107,6 +113,34 @@ public class FragmentMyPage extends Fragment implements FragmentMyPageView {
                 startActivity(intent);
             }
         });
+
+
+        ll_mypage_log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(view.getContext()) // TestActivity 부분에는 현재 Activity의 이름 입력.
+                        .setTitle("로그아웃")
+                        .setMessage("로그아웃 하시겠습니까?")
+                        .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                Intent intent = new Intent(viewGroup.getContext(), SignInActivity.class);
+                                startActivity(intent);
+                                ((Activity) getContext()).finish();
+
+                                Toast.makeText(getContext(), "로그아웃 처리가 완료되었습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+            }
+        });
+
     }
 
     private void tryGetMyPage() {
