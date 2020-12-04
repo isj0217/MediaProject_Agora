bee4c1ce06fe910c65d75a7e388f36dc96184e12
조인성 <insung@joinseong-ui-MacBookPro.local>
2020-12-04 22:01:24 +0900

    쪽지함이 비었을 때 보여줄 화면 구성 완료

--- app/src/main/java/com/example/mediaproject_agora/src/main/fragments/fragment_message/FragmentMessage.java
+++ app/src/main/java/com/example/mediaproject_agora/src/main/fragments/fragment_message/FragmentMessage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,8 @@ import com.example.mediaproject_agora.src.main.items.MessageRoomItem;
 
 import java.util.ArrayList;
 
+import static android.view.View.GONE;
+
 public class FragmentMessage extends Fragment implements FragmentMessageView{
     ViewGroup viewGroup;
 
@@ -25,10 +28,14 @@ public class FragmentMessage extends Fragment implements FragmentMessageView{
     private RecyclerView rv_message_box;
     private LinearLayoutManager linear_layout_manager;
 
+    private LinearLayout ll_message_empty;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.frag_message, container, false);
+
+        ll_message_empty = viewGroup.findViewById(R.id.ll_message_empty);
 
         rv_message_box = viewGroup.findViewById(R.id.rv_message_box_message_list);
 
@@ -76,20 +83,24 @@ public class FragmentMessage extends Fragment implements FragmentMessageView{
             case 100:
                 int num_of_message_rooms = messageRoomResponse.getMessageRoomResults().size();
 
-                for (int i = 0; i < num_of_message_rooms; i++){
-                    MessageRoomItem messageRoomItem = new MessageRoomItem();
+                if (num_of_message_rooms > 0) {
 
-                    messageRoomItem.setMessage_room_idx(messageRoomResponse.getMessageRoomResults().get(i).getMessage_room_idx());
-                    messageRoomItem.setUser_idx(messageRoomResponse.getMessageRoomResults().get(i).getUser_idx());
-                    messageRoomItem.setUser_nickname(messageRoomResponse.getMessageRoomResults().get(i).getUser_nickname());
-                    messageRoomItem.setMessage_idx(messageRoomResponse.getMessageRoomResults().get(i).getMessage_idx());
-                    messageRoomItem.setContent(messageRoomResponse.getMessageRoomResults().get(i).getContent());
-                    messageRoomItem.setTime(messageRoomResponse.getMessageRoomResults().get(i).getTime());
+                    ll_message_empty.setVisibility(GONE);
 
-                    m_message_room_item_list.add(messageRoomItem);
+                    for (int i = 0; i < num_of_message_rooms; i++) {
+                        MessageRoomItem messageRoomItem = new MessageRoomItem();
+
+                        messageRoomItem.setMessage_room_idx(messageRoomResponse.getMessageRoomResults().get(i).getMessage_room_idx());
+                        messageRoomItem.setUser_idx(messageRoomResponse.getMessageRoomResults().get(i).getUser_idx());
+                        messageRoomItem.setUser_nickname(messageRoomResponse.getMessageRoomResults().get(i).getUser_nickname());
+                        messageRoomItem.setMessage_idx(messageRoomResponse.getMessageRoomResults().get(i).getMessage_idx());
+                        messageRoomItem.setContent(messageRoomResponse.getMessageRoomResults().get(i).getContent());
+                        messageRoomItem.setTime(messageRoomResponse.getMessageRoomResults().get(i).getTime());
+
+                        m_message_room_item_list.add(messageRoomItem);
+                    }
+                    messageRoomAdapter.notifyDataSetChanged();
                 }
-                messageRoomAdapter.notifyDataSetChanged();
-
                 break;
 
             default:
